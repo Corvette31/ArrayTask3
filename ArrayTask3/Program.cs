6ea17dc0a9b5a352cd75a6f3fc1bfacb9b08e25e
Corvette31 <corvette.ss@yandex.ru>
2022-06-27 10:49:32 +0300

    внес исправления

--- ArrayTask3/Program.cs
+++ ArrayTask3/Program.cs
@@ -11,8 +11,7 @@ namespace ArrayTask3
             int itemMinValue = 1;
             int itemMaxValue = 101;
             int[] array = new int[arraySize];
-            bool isLocalMaximum;
-            int oneElement = 1;
+
 
             Console.WriteLine("Массив: \n");
 
@@ -24,38 +23,30 @@ namespace ArrayTask3
 
             Console.WriteLine("\n");
 
+            if (arraySize >= 2)
+            {
+                if (array[0] > array[1])
+                {
+                    Console.WriteLine($"Число {array[0]} индекс массива {0} является локальным максимумом");
+                }
+                if (array[arraySize - 1] > array[arraySize - 2])
+                {
+                    Console.WriteLine($"Число {array[arraySize - 1]} индекс массива {arraySize - 1} является локальным максимумом");
+                }
+            }
+
             for (int i = 0; i < arraySize; i++)
             {
-                isLocalMaximum = false;
-
-                if (i == 0 && i + oneElement > arraySize - oneElement)
-                {
-                    isLocalMaximum = true;
-                }
-                else if (i == 0 && i + oneElement <= arraySize - oneElement)
-                {
-                    if (array[i] > array[i + oneElement])
-                    {
-                        isLocalMaximum = true;
-                    }        
-                } 
-                else if (i == arraySize - oneElement && i - oneElement >= 0)
-                {
-                    if (array[i] > array[i - oneElement])
-                    {
-                        isLocalMaximum = true;
-                    }
-                }
-                else if (i > 0 && i < arraySize - oneElement)
-                {
-                    if (array[i] > array[i - oneElement] && array[i] > array[i + oneElement])
-                    {
-                        isLocalMaximum = true;
-                    }
-                }
-                if (isLocalMaximum)
+                if (i == 0 && i + 1 > arraySize - 1)
                 {
                     Console.WriteLine($"Число {array[i]} индекс массива {i} является локальным максимумом");
+                }
+                else if (i > 0 && i < arraySize - 1)
+                {
+                    if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                    {
+                        Console.WriteLine($"Число {array[i]} индекс массива {i} является локальным максимумом");
+                    }
                 }
             }
         }
